7e8144dfa6c9d3142722d0b06162921717055956
Matt Soucoup <msoucoup@codemilltech.com>
2015-10-14 19:57:33 -0500

    Adding constants

---
+++ CheesedStorage/Helpers/AzureConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CheesedStorage.Local
+{
+	public static class AzureConstants
+	{
+		public static readonly string MobileServiceUrl = "";
+		public static readonly string MobileServiceAppKey = "";
+		public static readonly string MobileServiceAPISasName = "";
+		public static readonly string StoragePhotoContainerUrl = "";
+		public static readonly string StorageAudioContainerUrl = "";
+		public static readonly string StoragePhotoContainerName = "";
+		public static readonly string StorageAudioContainerName = "";
+
+		public static readonly string PhotoStorageComplete = "photostoragecomplete";
+		public static readonly string AudioStorageComplete = "audiostoragecomplete";
+	}
+}
+
